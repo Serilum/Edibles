441b5820fcd2b6474fb57a2b0fcd406c700ea38f
Rick South <rick@natamus.com>
2023-12-12 10:25:22 +0100

    Edibles source code for 1.20.4.

---
+++ NeoForge/src/main/java/com/natamus/edibles/neoforge/events/NeoForgeEdibleEvent.java
@@ -0,0 +1,21 @@
+package com.natamus.edibles.neoforge.events;
+
+import com.natamus.edibles.events.EdibleEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeEdibleEvent {
+	@SubscribeEvent
+	public static void onBlockRightClick(PlayerInteractEvent.RightClickBlock e) {
+		if (!EdibleEvent.onBlockRightClick(e.getLevel(), e.getEntity(), e.getHand(), e.getPos(), e.getHitVec())) {
+			e.setCanceled(true);
+		}
+	}
+	
+	@SubscribeEvent
+	public static void onClickEmpty(PlayerInteractEvent.RightClickItem e) {
+		EdibleEvent.onClickEmpty(e.getEntity(), e.getLevel(), e.getHand());
+	}
+}
