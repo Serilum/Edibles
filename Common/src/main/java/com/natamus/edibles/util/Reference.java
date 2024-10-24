c80607f453b1497d6f513339a7066916a5c11243
Rick South <rick@natamus.com>
2024-10-24 20:03:09 +0200

    Edibles source code for 1.21.2.

---
+++ Common/src/main/java/com/natamus/edibles/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.edibles.util;
+
+public class Reference {
+	public static final String MOD_ID = "edibles";
+	public static final String NAME = "Edibles";
+	public static final String VERSION = "4.4";
+	public static final String ACCEPTED_VERSIONS = "[1.21.2]";
+}
