5666c22edae16bc7b2c15d3d1fcd66e18dbd051f
Rick South <rick@natamus.com>
2026-09-22 16:51:48 +0200

    Updated Edibles to Minecraft version 26.3.0.

--- Common/src/main/java/com/natamus/edibles/events/EdibleEvent.java
+++ Common/src/main/java/com/natamus/edibles/events/EdibleEvent.java
@@ -12,6 +12,7 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.component.SwingAnimation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -139,7 +140,7 @@ public class EdibleEvent {
 			return InteractionResult.PASS;
 		}
 		
-		player.swing(hand);
+		player.swingAndResetAttackStrength(hand, SwingAnimation.DEFAULT, false);
 		
 		Util.setPlayerUse(playername);
 		if (!player.isCreative()) {
